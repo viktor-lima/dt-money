f08335d33668440ecaaa0930eb4eae067c51712f
viktor-lima <joaolimak10@outlook.com>
2025-02-10 21:59:43 -0300

    feat: styled-components -> defaultTheme and GlobalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyled } from "./styles/global";
 
 
 export function App() {
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyled />
       <h1>Helo world</h1>
-    </>
+    </ThemeProvider>
   )
 }

ef2880f01d3530a89807369e6f34812a296b925c
viktor-lima <joaolimak10@outlook.com>
2025-02-17 23:41:05 -0300

    lib -> radix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 import logoImg from '../../assets/logo.svg'
-
+import * as Dialog from '@radix-ui/react-dialog'
 
 export function Header () {
   return (
@@ -10,8 +10,22 @@ export function Header () {
         <HeaderContent>
 
           <img src={logoImg} />
+          
+          <Dialog.Root>
+            <Dialog.Trigger asChild> 
+              <NewTransactionButton>Nova Transação</NewTransactionButton>
+            </Dialog.Trigger>
 
-          <NewTransactionButton> Nova Transação</NewTransactionButton>
+            <Dialog.Portal>
+              <Dialog.Overlay />
+
+              <Dialog.Content>
+                <Dialog.Title>Nova Transação</Dialog.Title>
+                <Dialog.Close />
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
+
         </HeaderContent>
       </HeaderContainer>
     </div>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+
 export const HeaderContainer = styled.header`
 
   background: ${(props) => props.theme["gray-900"]};

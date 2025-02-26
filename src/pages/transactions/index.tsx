6f3a19881a028b338ffe13971418c9927b083aa1
viktor-lima <joaolimak10@outlook.com>
2025-02-25 23:18:23 -0300

    feat: first required API

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHigthlight, TransactionsContainer, TransactionsTable } from "./styles";
 
+interface Transaction {
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  category: string;
+  price: number;
+  createdAt: string;
+}
+
+
 export function Transactions () {
+
+  const [ transactions, setTransactions ] = useState<Transaction[]>([])
+
+  async function loadTransactions () {
+    const response = await fetch('http://localhost:3333/transactions')
+    const data = await response.json();
+    setTransactions(data)
+  }
+
+  useEffect(() => {
+    loadTransactions()
+  }, [])
+
   return (
     <div>
       <Header />
@@ -14,38 +38,20 @@ export function Transactions () {
         <SearchForm />
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHigthlight variante="income">
-                    R$ 12.000,00
-                </PriceHigthlight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHigthlight variante="income">
-                    R$ 12.000,00
-                </PriceHigthlight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHigthlight variante="outcome">
-                    - R$ 12.000,00
-                </PriceHigthlight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
+            {transactions?.map(row => {
+              return (
+                <tr key={row.id}>
+                  <td width="50%">{row.description}</td>
+                  <td>
+                    <PriceHigthlight variante={row.type}>
+                        {row.price}
+                    </PriceHigthlight>
+                  </td>
+                  <td>{row.type}</td>
+                  <td>{row.createdAt}</td>
+                </tr>
+              )
+            })}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
